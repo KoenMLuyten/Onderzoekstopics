f401116c7941be4a1ec8a833a8266385d3a995ad
Koen Luyten <Koen.Luyten@student.kdg.be>
2018-03-01 01:02:56 +0100

    Moq creates en reads geimplementeerd

---
+++ BLTest/TicketManagerNUnit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Moq;
+using SC.DAL;
+using SC.BL;
+using SC.BL.Domain;
+
+namespace BLTest
+{
+    [TestFixture]
+    class TicketManagerNUnit
+    {
+        Mock<ITicketRepository> mockRepo = new Mock<ITicketRepository>();
+        TicketManager sut;
+
+        [OneTimeSetUp]
+        public void MockSetup()
+        {
+            //Create operations simply return te given object
+            mockRepo.Setup(repo => repo.CreateTicket(It.IsAny<Ticket>())).Returns(((Ticket t) => t));
+            mockRepo.Setup(repo => repo.CreateTicketResponse(It.IsAny<TicketResponse>())).Returns((TicketResponse t) => t);
+
+            //List read operations return a list counting five of the requested object
+            //Ticketresponses include a Ticket with the requeste TicketID
+            List<TicketResponse> testResponses = new List<TicketResponse>();
+            Ticket testTicket = new Ticket();
+            for (int i = 0; i < 5; i++)
+            {
+                TicketResponse tr = new TicketResponse();
+                tr.Ticket = testTicket;
+                testResponses.Add(tr);
+            }
+            mockRepo.Setup(repo => repo.ReadTicketResponsesOfTicket(It.Is<int>(i => i != 0))).Callback((int i) => testResponses.ForEach(tr => tr.Ticket.TicketNumber = i)).Returns(testResponses);
+            mockRepo.Setup(repo => repo.ReadTicketResponsesOfTicket(It.Is<int>(i => i == 0))).Returns((new List<TicketResponse>()));
+
+
+            List<Ticket> testTickets = new List<Ticket>();
+            for (int i = 0; i < 5; i++)
+            {
+                testResponses.Add(new Ticket());
+            }
+            mockRepo.Setup(repo => repo.ReadTickets()).Returns(testTickets);
+
+            //Returns a ticket with the requested ID
+            mockRepo.Setup(repo => repo.ReadTicket(It.IsAny<int>())).Callback((int i) => testTicket.TicketNumber = i).Returns(testTicket);
+
+
+            ITicketRepository testRepo = mockRepo.Object;
+            sut = new TicketManager(testRepo);
+        }
+
+
+    }
+}
--- BLTest/TicketManagerTest.cs
+++ BLTest/TicketManagerTest.cs
@@ -19,9 +19,11 @@ namespace BLTest
         public void Setup()
         {
             mockRepo.Setup(repo => repo.CreateTicket(It.IsAny<Ticket>())).Returns(((Ticket t) => t));
+            mockRepo.Setup(repo => repo.CreateTicketResponse(It.IsAny<TicketResponse>())).Returns((TicketResponse t) => t);
+            mockRepo.Setup(repo => repo.ReadTicketResponsesOfTicket(It.IsAny<int>())).Returns((List<TicketResponse> t) => t);
             ITicketRepository testRepo = mockRepo.Object;
             sut = new TicketManager(testRepo);
-        }   
+        }
 
         [TestMethod]
         public void AddTicketTest()
@@ -29,8 +31,15 @@ namespace BLTest
 
             int accountid = 1;
             string question = "ik heb een probleem";
-
+            Ticket newTicket = new Ticket()
+            {
+                AccountId = accountid,
+                Text = question
+            };
+        
             Ticket returnedTicket = sut.AddTicket(accountid, question);
+
+            mockRepo.Verify(repo => repo.CreateTicket(newTicket), Times.Exactly(1));
             Assert.AreEqual(accountid, returnedTicket.AccountId);
             Assert.AreEqual(question, returnedTicket.Text);
 
