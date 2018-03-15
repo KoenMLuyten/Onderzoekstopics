f6de9f00c07127b11dec34386408beab18e269cd
Koen Luyten <Koen.Luyten@student.kdg.be>
2018-03-15 18:47:50 +0100

    Final Commit

--- UI-MVC/App_Start/UnityConfig.cs
+++ UI-MVC/App_Start/UnityConfig.cs
@@ -39,11 +39,7 @@ namespace SC.UI.Web.MVC
         /// </remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below.
-            // Make sure to add a Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
-
-            // TODO: Register your type's mappings here.
+           
             container.RegisterType<ITicketManager, TicketManager>();
             container.RegisterType<ITicketRepository, TicketRepository>();
         }
